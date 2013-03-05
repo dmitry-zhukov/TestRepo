81b3ca927b11786c26d2f21e1d06025c09f5e889
dmitry.zhukov <zhukov@d-zhukov.local>
2013-03-05 20:51:23 +0400

    unit tests + project changes

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  GuildBrowserLogicTests.h
+//  GuildBrowserLogicTests
+//
+//  Created by dmitry.zhukov on 05/03/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GuildBrowserLogicTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by dmitry.zhukov on 05/03/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
